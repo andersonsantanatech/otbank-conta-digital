e2cfc501f144e098d32ddef4bf715c55e16ffed1
andersonzup <anderson.santana@zup.com.br>
2021-10-04 11:23:54 -0300

    Commit spring project

---
+++ src/main/java/br/zup/com/otbank/OtbankApplication.java
@@ -0,0 +1,13 @@
+package br.zup.com.otbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/zup/com/otbank/OtbankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.zup.com.otbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
